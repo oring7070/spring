aae0fba6a77bbbc50432abde343a163b209ce677
LEE MIN WO0 <zizon_2710@naver.com>
2022-07-04 15:03:19 +0900

    commit mvc

--- src/main/java/com/example/demo/ConTroller.java
+++ src/main/java/com/example/demo/ConTroller.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConTroller {
     @GetMapping("/hello")
-    public String home() {
-        return "Hello Spring Boot";
+    public String hello(Model model) {
+        model.addAttribute("data","hello!!");
+        return "hello";
     }
 }
